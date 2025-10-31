d36d56a240bfcec34f695ba618b2f1037b3d1756
Veck404 <victor7ultimate@gmail.com>
2025-10-31 22:48:11 +0100

    fixed cart

--- src/store/Constants.js
+++ src/store/Constants.js
@@ -4,14 +4,19 @@ import twentyFiveLitres from "../Svgs/25litres.png";
 
 export const Products = [
   // Home categories
-  { src: fiveLitres, type: "newarrival", name: "5 Litres Palm Oil", stars: 4.6, cost: 5500, discount: 0 },
-  { src: tenLitres, type: "topselling", name: "10 Litres Palm Oil", stars: 4.8, cost: 12000, discount: 5 },
-  { src: twentyFiveLitres, type: "onsale", name: "25 Litres Palm Oil", stars: 4.7, cost: 28500, discount: 12 },
+  // { src: fiveLitres, type: "newarrival", name: "5 Litres Palm Oil – Chef’s Reserve", stars: 4.9, cost: 6000, discount: 4 },
+  { src: tenLitres, type: "topselling", name: "10 Litres Palm Oil – Golden Batch", stars: 4.7, cost: 11800, discount: 6 },
+  { src: twentyFiveLitres, type: "onsale", name: "25 Litres Palm Oil – Export Grade", stars: 4.8, cost: 27900, discount: 15 },
 
   // Shop items
-  { src: fiveLitres, id: "5", place: "shop", type: "newarrival", type2: "All", name: "5 Litres Palm Oil", stars: 4.6, cost: 5500, discount: 0 },
-  { src: tenLitres, id: "10", place: "shop", type: "topselling", type2: "All", name: "10 Litres Palm Oil", stars: 4.8, cost: 12000, discount: 5 },
-  { src: twentyFiveLitres, id: "25", place: "shop", type: "onsale", type2: "All", name: "25 Litres Palm Oil", stars: 4.7, cost: 28500, discount: 12 },
+  { src: fiveLitres, id: "5B", place: "shop", type: "newarrival", type2: "All", name: "5 Litres Palm Oil – Family Pack", stars: 4.7, cost: 5650, discount: 2 },
+  { src: fiveLitres, id: "5C", place: "shop", type: "newarrival", type2: "All", name: "5 Litres Palm Oil – Organic Blend", stars: 4.8, cost: 6100, discount: 5 },
+
+  { src: tenLitres, id: "10B", place: "shop", type: "topselling", type2: "All", name: "10 Litres Palm Oil – Signature Batch", stars: 4.9, cost: 12250, discount: 6 },
+  { src: tenLitres, id: "10C", place: "shop", type: "topselling", type2: "All", name: "10 Litres Palm Oil – Premium Cold Pressed", stars: 5, cost: 13000, discount: 8 },
+
+  { src: twentyFiveLitres, id: "25B", place: "shop", type: "onsale", type2: "All", name: "25 Litres Palm Oil – HoReCa Pack", stars: 4.9, cost: 29000, discount: 14 },
+  { src: twentyFiveLitres, id: "25C", place: "shop", type: "onsale", type2: "All", name: "25 Litres Palm Oil – Export Series", stars: 4.9, cost: 29500, discount: 16 },
 ];
 
 export const SiteReviews = [
